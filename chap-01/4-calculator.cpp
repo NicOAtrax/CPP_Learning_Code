902c4ceb538835eabd8acefd7b85e5128af32495
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-01-12 04:40:03 +0100

    Calcularor Completed

--- chap-01/4-calculator.cpp
+++ chap-01/4-calculator.cpp
@@ -2,65 +2,109 @@
 #include <string>
 #include <vector>
 
-int main(int argc, char** argv)
+int add(const std::vector<int>& values)
 {
-    // Parsing program parameters.
+    int result = 0;
+    for (auto v : values)
+    {
+        result += v;
+    }
+    return result;
+}
+
+int multiply(const std::vector<int>& values)
+{
+    auto result = 1;
+    for (auto v : values)
+    {
+        result *= v;
+    }
+    return result;
+}
+
+int sub(const std::vector<int>& values)
+{
+    auto result = values[0];
+    for (auto i = 1u; i < values.size(); ++i)
+    {
+        result -= values[i];
+    }
+    return result;
+}
+
+bool parse_params(char& op, std::vector<int>& values, int argc, char** argv)
+{
+
     if (argc < 2)
     {
         std::cerr << "Expected operator as first argument." << std::endl;
-        return -1;
+        return false;
     }
 
-    std::string op = argv[1];
+    std::string op_str = argv[1];
 
-    if (op != "+" && op != "*" && op != "-")
+    if (op_str != "+" && op_str != "*" && op_str != "-")
     {
         std::cerr << "Expected operator to be '+', '*' or '-'." << std::endl;
-        return -1;
+        return false;
     }
 
-    std::vector<int> values;
+    op = op_str[0];
+
     for (auto arg_i = 2; arg_i < argc; ++arg_i)
     {
         auto value = std::stoi(argv[arg_i]);
         values.emplace_back(value);
     }
 
-    // Process operation, depending on the operator.
+    if (op == '-' && values.empty())
+    {
+        std::cerr << "Operator '-' expects at least one operand to substract from." << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+int compute_result(char op, const std::vector<int>& values)
+{
     auto result = 0;
 
-    if (op == "+")
+    switch (op)
     {
-        for (auto v : values)
-        {
-            result += v;
-        }
+    case '+':
+        return add(values);
+    case '*':
+        return multiply(values);
+    case '-':
+        return sub(values);
     }
-    else if (op == "*")
-    {
-        result = 1;
-        for (auto v : values)
-        {
-            result *= v;
-        }
-    }
-    else if (op == "-")
-    {
-        if (values.empty())
-        {
-            std::cerr << "Operator '-' expects at least one operand to substract from." << std::endl;
-            return -1;
-        }
 
-        result = values[0];
-        for (auto i = 1u; i < values.size(); ++i)
-        {
-            result -= values[i];
-        }
+    return result;
+}
+
+void display_result(int result)
+{
+    std::cout << "Result is " << result << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+
+    // Parsing program parameters.
+    char             op = '?';
+    std::vector<int> values;
+
+    if (!parse_params(op, values, argc, argv))
+    {
+        return -1;
     }
+
+    // Process operation, depending on the operator.
+    int result = compute_result(op, values);
 
     // Output result.
-    std::cout << "Result is " << result << std::endl;
+    display_result(result);
 
     return 0;
 }

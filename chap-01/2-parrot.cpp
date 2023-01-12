adfd0bb50d4ea9bff5394f317c1291a5a939a35b
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-01-12 02:41:54 +0100

    Parrot completed

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,38 @@
-// Implement the program here.
+#include <iostream>
+#include <string>
+
+int main()
+{
+    /*
+    int number = 0;
+    std::cin >> number;
+    do
+    {
+        std::cout << "Craow " << number << " !" << std::endl;
+        std::cin >> number;
+    }
+    while (number != -1);
+    return 0;
+    */
+
+    /*
+    int number = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        std::cin >> number;
+        if (number == -1)
+            break;
+        std::cout << "Craow " << number << " !" << std::endl;
+    };
+    */
+
+    using namespace std;
+
+    string word;
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> word;
+        cout << "Craow " << word << " !" << endl;
+    };
+    return 0;
+}

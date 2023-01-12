09006ae130db08e766153f4a7930c8ce65ce897d
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-01-12 03:18:10 +0100

    Array Completed

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
+#include <vector>
 
-int main()
+int main(int argc, char** argv)
 {
-    int array[] = { 0, 1, 2, 3 };
+    using namespace std;
 
-    for (int i = 0; i < 4; ++i)
+    /*
+    int size        = 50;
+    int array[size] = {};
+    for (int i = 0; i < size; i++)
     {
-        std::cout << array[i] << std::endl;
+        array[i] = i + 1;
+    }
+
+    for (int n : array)
+    {
+        cout << n << endl;
+    }
+    */
+
+    if (argc < 2)
+    {
+        cerr << "Missing required argument(s) !" << endl;
+        return -1;
+    }
+
+    int array_size;
+    sscanf(argv[1], "%d", &array_size);
+
+    vector<int> array;
+    for (int i = 0; i < array_size; i++)
+    {
+        array.emplace_back(i + 1);
+    }
+
+    for (int n : array)
+    {
+        cout << n << endl;
     }
 
     return 0;
